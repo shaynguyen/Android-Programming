3244fa821e7db6bc53e4c64165002d09ff137cfb
shay nguyen <shay.nguyen@gmail.com>
2020-11-16 18:11:55 -0800

    1: Your first application

---
+++ geoquiz/README.md
@@ -0,0 +1,5 @@
+# GeoQuiz
+A simple true/false quiz
+
+## 1: Your first applicaton
+* Create a new project
---
+++ geoquiz/src/main/java/com/bignerdranch/android/geoquiz/GeoQuiz.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeoQuiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geo_quiz)
+    }
+}
