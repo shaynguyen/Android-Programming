c14db873b1f752ab06a64e9f42d4e347d32365df
shay nguyen <shay.nguyen@gmail.com>
2020-11-16 20:54:18 -0800

    2: Challenges
    1) Clicking on the question will move to the next question
    2) Add a previous button (go back)
    3) Replace prev/next button with ImageButton

--- geoquiz/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ geoquiz/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: ImageButton
+
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -31,7 +34,13 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        prevButton = findViewById(R.id.prev_button)
         nextButton = findViewById(R.id.next_button)
+
+        questionTextView.setOnClickListener { view: View ->
+            nextQuestion()
+            updateQuestion()
+        }
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
@@ -41,8 +50,13 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
 
+        prevButton.setOnClickListener {
+            prevQuestion()
+            updateQuestion()
+        }
+
         nextButton.setOnClickListener { view: View ->
-            currentIndex = (currentIndex + 1) % questionBank.size
+            nextQuestion()
             updateQuestion()
         }
 
@@ -55,10 +69,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        if(userAnswer == questionBank[currentIndex].answer) {
+        if (userAnswer == questionBank[currentIndex].answer) {
             Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
         } else {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT) .show()
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun prevQuestion() {
+        currentIndex = if (currentIndex - 1 < 0) {
+            questionBank.size - 1
+        } else {
+            currentIndex - 1
+        }
+    }
+    private fun nextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
     }
 }
