79e90414840ca1df39d1d7364fe1aaf1c004335b
shay nguyen <shay.nguyen@gmail.com>
2020-11-16 23:54:05 -0800

    3: Logging the activity lifecycles

--- geoquiz/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ geoquiz/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -28,6 +31,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d(TAG, "onCreated")
+
         questionTextView = findViewById(R.id.question_text_view)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -47,6 +52,32 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 
     private fun updateQuestion() {
